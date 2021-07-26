e84c93c8b9f19993d1fe43045c28b64f14cad039
Caleb P. Nwokocha <nwo20001@byui.edu>
2021-07-26 17:10:26 -0500

    Added stub functions

--- src/RandomNumGen.java
+++ src/RandomNumGen.java
@@ -1,2 +1,16 @@
 public class RandomNumGen {
+    private int seed;
+    private int min;
+    private int max;
+    private int generatedNum[] = {};
+
+    public RandomNumGen(int seed, int min, int max) {
+        this.seed = seed;
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getNum() {
+
+    }
 }
