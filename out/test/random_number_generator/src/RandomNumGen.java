85970a1d5b036ff60c9cab8a3a98f22904fb1300
Caleb P. Nwokocha <nwo20001@byui.edu>
2021-07-26 19:54:24 -0500

    Minor correction in comment

--- out/test/random_number_generator/src/RandomNumGen.java
+++ out/test/random_number_generator/src/RandomNumGen.java
@@ -18,13 +18,25 @@ public class RandomNumGen {
 
         while (true) {
             for (int i = 0; i < generatedNum.length; i++) {
+
+                // Min and Max Definition:
+                // Given a set R of random numbers, R[1]...R[n], R[n] is
+                // within min and max threshold iff min <= R[n] <= max.
                 if (generatedNum[i] >= min && generatedNum[i] <= max) {
+
+                    // Program Output Definition:
+                    // The first discovered min <= R[n] <= max in R is the output.
                     contraintedNum = generatedNum[i];
                     return contraintedNum;
                 } else {
                     for(int j = 0; j <= 9; j++) {
+
+                        // X[n+1] = ((a * X[n]) + c) mod m
                         seed = ((MULTIPLIER * seed) + INCREMENT) % MODULUS;
                         System.out.print("New seed is " + seed + "\n");
+
+                        // Store seed in the generatedNum array, R, to check
+                        // for min <= R[n] <= max.
                         generatedNum[j] = seed;
                     }
                 }
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         // NOTE: Some input values, for example seed = 7, min = 9, max = 11,
-        // don't have a solution for random number with the range(min, max).
+        // don't have a solution for random number within the range(min, max).
         RandomNumGen randomNumGen = new RandomNumGen(9, 9, 11);
         System.out.println("\nRandom number within range is " + randomNumGen.getNum());
     }
